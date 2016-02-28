9bfc208996f197b764a51a50051e2f18611394c2
Manu PK <manupk.tech@gmail.com>
2016-02-28 15:08:29 +0530

    Made the packaging to WAR

--- expenser/src/main/java/com/manu/expenser/boot/ExpenserApplication.java
+++ expenser/src/main/java/com/manu/expenser/boot/ExpenserApplication.java
@@ -2,13 +2,20 @@ package com.manu.expenser.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.manu.expenser" })
-public class ExpenserApplication {
+public class ExpenserApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpenserApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExpenserApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ExpenserApplication.class, args);
+    }
 }
